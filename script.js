be685e30c66544bd14e8d8b19fc2383748940601
Iftikhar uddin <iftipro@gmail.com>
2016-08-24 22:18:37 +0500

    Marquee shit

--- script.js
+++ script.js
@@ -8,11 +8,24 @@ var ifti = {
 };
 
 function welcome(person){
-	console.log("Hi " + person.love);
+	document.write("<marquee> Hi " + person.love );
+	document.write("</marquee>");
+}
+function welcome2(person){
+	document.write("<marquee direction='right' style='color:green'> Hi " + person.love );
+	document.write("</marquee>");
 }
 welcome(ifti);
 welcome({
 	love: "Olamba",
 	passion:"JavaScript"
 });
+
+for(i = 0; i<=10; i++){
+	if(i%2 === 0){
+		welcome2(ifti);
+	}else{
+		welcome(ifti);
+	}
+}
 

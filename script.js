4cdd909ca33a0a470b032787d1cb958a29358e03
Iftikhar uddin <iftipro@gmail.com>
2016-08-25 18:07:52 +0500

    JSON and Object Literals PLUS Stringify and JSONdotparse

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
-var ifti = {
-	passion: "web",
-	love: "Salena Gomez",
-	address:{
-		street: "23 House dash",
-		name: "Tony Brown"
-	}
+var person={
+	firstname: 'ifti',
+	isProgrammer: true
 };
 
-function welcome(person){
-	document.write("<marquee> Hi " + person.love );
-	document.write("</marquee>");
-}
-function welcome2(person){
-	document.write("<marquee direction='right' style='color:green'> Hi " + person.love );
-	document.write("</marquee>");
-}
-welcome(ifti);
-welcome({
-	love: "Olamba",
-	passion:"JavaScript"
-});
+console.log(JSON.stringify(person));
 
-for(i = 0; i<=10; i++){
-	if(i%2 === 0){
-		welcome2(ifti);
-	}else{
-		welcome(ifti);
-	}
-}
+var JSONObj = JSON.parse('{"firstname": "ifti","isProgrammer":true}');
 
+console.log(JSONObj);

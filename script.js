ba6509ff5113286dda042b79aa2c2fa5125b7474
Iftikhar uddin <iftipro@gmail.com>
2016-08-27 20:08:31 +0500

    Closures

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ var another = buildFunctions();
 another[0]();
 another[1]();
 another[2]();
+
+//Understanding Closures
+function buildFunctions2(){
+	//empty array
+	var arr = [];
+	
+	for(i = 0; i < 3; i++){
+		arr.push(
+			(function(j){
+				return function(){
+					console.log(j);
+				}
+			}(i))
+		)
+	}
+	return arr;
+	
+}
+
+var another1 = buildFunctions2();
+another1[0]();
+another1[1]();
+another1[2]();

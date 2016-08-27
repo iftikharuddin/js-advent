0b963ff23ba0be9d9ad2e03666725e5ec23f59a3
Iftikhar uddin <iftipro@gmail.com>
2016-08-27 19:56:20 +0500

    Understanding and playing with JavaScript Closures

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-//using an Immediately Invoked Function Expression (IIFE)s
-var funk = function(name){
-	console.log("Hey " + name);
-}('Ehehehe');
+//Understanding Closures
+function buildFunctions(){
+	//empty array
+	var arr = [];
+	
+	for(i = 0; i < 3; i++){
+		arr.push(
+			function(){
+				console.log(i);
+			}
+		)
+	}
+	return arr;
+	
+}
 
-//JavaScript is cool
-(function(name){
-	console.log("Hey " + name);
-}('Tony is awesome')); //IIFE trick
+var another = buildFunctions();
+another[0]();
+another[1]();
+another[2]();

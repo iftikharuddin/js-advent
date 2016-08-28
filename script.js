b7551e856c9caedc199629ba89f734e6d9fa58f1
Iftikhar uddin <iftipro@gmail.com>
2016-08-28 22:24:19 +0500

    Functions fucx

--- script.js
+++ script.js
@@ -1,43 +1,12 @@
-//Understanding Closures
-function buildFunctions(){
-	//empty array
-	var arr = [];
-	
-	for(i = 0; i < 3; i++){
-		arr.push(
-			function(){
-				console.log(i);
-			}
-		)
-	}
-	return arr;
-	
+//function 
+
+var test = function(){
+	console.log('hey there');
 }
 
-var another = buildFunctions();
-another[0]();
-another[1]();
-another[2]();
+test();
 
-//Understanding Closures
-function buildFunctions2(){
-	//empty array
-	var arr = [];
-	
-	for(i = 0; i < 3; i++){
-		arr.push(
-			(function(j){
-				return function(){
-					console.log(j);
-				}
-			}(i))
-		)
-	}
-	return arr;
-	
-}
-
-var another1 = buildFunctions2();
-another1[0]();
-another1[1]();
-another1[2]();
+// Invoked on the fly
+(function cool(){
+	console.log('hay man');
+}())

3b0e60ee45161e33cc5c4d04a9e9b23797b687fc
Iftikhar uddin <iftipro@gmail.com>
2016-09-15 17:48:02 +0500

    Creating Empty object and adding default data to its prototype via object dot create method

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// Don't use For in with arrays it will display the attached property in Prototype in result 
-Array.prototype.hello = 'GitHub';
-var arr = ['ifti', 'john', 'doe'];
+// Polly fill and Creating Object in a new way
 
-for(var i=0; i< arr.length; i++){
-	console.log(i + ': ' + arr[i])
+var person = {
+	firstname : 'Iftikhar',
+	lastname: 'uddin',
+	welcome: function(){
+		return 'Hello' + this.firstname;
+	}
 }
 
-for(var i in arr){
-	console.log(i + ': ' + arr[i])
-}
+var ifti = Object.create(person);
+console.log(ifti);

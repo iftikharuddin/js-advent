48830dfb0f16025af7ac1fce41ef950a511e8a33
Iftikhar uddin <iftipro@gmail.com>
2016-09-24 18:15:02 +0500

    Razam Munz time

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //Safe Code for our custom js framework
 (function(global, $){
 	
+	var Greetr = function(firstName, lastName, language){
+		return new Greetr.init(firstName, lastName, language);
+	}
+	
+	Greetr.init = function(firstName, lastName, language){
+		
+	}
 	
 }(window, jQuery));
